33e7f2b66e835dc2dd29f8dab87398aefc3527f3
rwitczyk <Robert123>
2019-10-07 00:06:59 +0200

    Poprawki w wydarzeniach w kalendarzu. Nowe style kalendarza

--- src/app/components/doctor-visits-date-picker/doctor-visits-date-picker.component.ts
+++ src/app/components/doctor-visits-date-picker/doctor-visits-date-picker.component.ts
@@ -12,16 +12,34 @@ export class DoctorVisitsDatePickerComponent implements OnInit {
   calendarPlugins = [dayGridPlugin, interactionPlugin];
   calendarEvents = [];
   eventMyStyle = 'my-calendar-event-style';
+  actualDate: Date;
+  actualDay: string;
+  actualMonth: string;
+  actualYear: string;
 
   constructor() {
   }
 
   ngOnInit() {
+    this.actualDate = new Date();
+
+    for (let i = 0; i < 30; i++) {
+      this.actualDay = (this.actualDate.getDate() + i).toString();
+      this.actualMonth = (this.actualDate.getMonth() + 1).toString();
+      this.actualYear = (this.actualDate.getFullYear()).toString();
+
+      if (this.actualDay.toString().length < 2) {
+        this.actualDay = '0' + this.actualDay.toString();
+      }
+
+      this.calendarEvents.push({
+        title: '8:00 - 14:00', date: this.actualYear + '-' + this.actualMonth + '-' + (this.actualDay)
+      });
+    }
   }
 
   handleDateClick(arg) {
-    this.calendarEvents.push({
-      title: '8:00 - 14:00', date: arg.dateStr
-    });
+    console.log(arg.dateStr);
+    // navigate to another component
   }
 }
